ba0744f47036fdc6d103e1d0ea33b354390f748d
skipper8 <mharper48@gmail.com>
2021-06-01 12:07:12 -0700

    website

--- website/website.py
+++ website/website.py
@@ -6,10 +6,30 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-#@app.route('/library')
-#def library():
-    #return render_template('library.html')
+@app.route('/library')
+def library():
+    return render_template('library.html')
 
-#@app.route('/wishlist')
-#def wishlist():
-    #return render_template('wishlist.html')
+@app.route('/library/books')
+def books():
+    return render_template('books.html')
+
+@app.route('/library/music')
+def music():
+    return render_template('music.html')
+
+@app.route('/library/games')
+def games():
+    return render_template('games.html')
+
+@app.route('/library/movies')
+def movies():
+    return render_template('movies.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/papers')
+def papers():
+    return render_template('papers.html')
